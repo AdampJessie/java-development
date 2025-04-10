d928c12e39b841e0f2f458c4f1dbf8416db38945
unknown <adampjessie@gmail.com>
2025-04-10 09:28:33 -0400

    Initial commit, Workbook-1 and .gitignore

---
+++ workbook-1/projShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+
+        System.out.println("_________________");
+        System.out.println("| Shopping List |");
+        System.out.println("|_______________|");
+        System.out.println("| Eggs\t\t\t|\n| Milk\t\t\t|\n| Bread\t\t\t|\n| Chocolate\t\t|\n| Firewood\t\t|");
+
+        System.out.println("| Phone Charger |");
+        System.out.println("| Cat Food\t\t|");
+        System.out.println("| Dog Food\t\t|");
+        System.out.println("| Steak\t\t\t|");
+        System.out.println("| Seasoning\t\t|");
+        System.out.println("|_______________|");
+    }
+
+}
