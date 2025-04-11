47b5fe34a49769c4f8cc086b837154856b766912
unknown <adampjessie@gmail.com>
2025-04-11 18:11:14 -0400

    Updated Payroll Calculator with overtime and formatting

--- workbook-1/PayrollCalculator/src/main/java/com/pluralsight/PayCalc.java
+++ workbook-1/PayrollCalculator/src/main/java/com/pluralsight/PayCalc.java
@@ -7,6 +7,7 @@ public class PayCalc {
 
         Scanner payScan = new Scanner(System.in);
 
+
         // Prompt and store user input for name, hoursWorked, and payRate.
         System.out.print("What's your name?: ");
         String name = payScan.nextLine();
@@ -17,12 +18,33 @@ public class PayCalc {
         System.out.print("How much $ do you make an hour?: ");
         double payRate = payScan.nextFloat();
 
-        // Calculate gross pay
-        double payGross = (hoursWorked * payRate);
+        // Calculate gross pay and overtime using simple if statement. Store a string displaying whether user worked overtime.
+        double payGross;
+        String checkOT;
 
-        // Print formatted message containing all user input and calculated gross pay
-        System.out.printf("Hello, %s, you have worked %.0f hours at a rate of $%.2f per hour.\nYour gross pay is %.2f",
-                name, hoursWorked, payRate, payGross);
+        if (hoursWorked > 40) {
+            double hoursOver = hoursWorked - 40;
+            payGross = (40 * payRate) + (hoursOver * (payRate*1.5));
+            checkOT = String.format("You worked %.0f hours of overtime.", hoursOver);
+        }
+        else {
+            payGross = (hoursWorked * payRate);
+            checkOT = ("You did not work any overtime.");
+        }
+
+        // Format message containing all user input, overtime, and calculated gross pay - then output message.
+
+        String payRoll = """
+                +---------------------------------------+
+                | Hello, %s!\t\t\t\t\t\t\t|
+                | Worked %.0f hours at $%.2f per hour.\t|
+                | %s\t\t|
+                +---------------------------------------+
+                | Your gross pay is $%.2f\t\t\t|
+                +---------------------------------------+
+            """.formatted(name, hoursWorked, payRate, checkOT, payGross);
+        System.out.println(payRoll);
+
 
     }
 }
