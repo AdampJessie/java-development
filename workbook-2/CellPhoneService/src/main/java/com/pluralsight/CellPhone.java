82c2259179317ed8167534d3f09e72c0e8000522
unknown <adampjessie@gmail.com>
2025-04-18 11:49:51 -0400

    Completed CellPhone exercise

---
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,47 @@
+package com.pluralsight;
+
+public class CellPhone {
+
+    private String model = "" , carrier = "" , phoneNumber = "" , owner = "";
+    private int serialNumber = 0;
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
---
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+
+    public static void main(String[] args) {
+        Scanner phoneInput = new Scanner(System.in);
+
+
+        CellPhone phone1 = new CellPhone(); // Creating a new CellPhone class object.
+        StringBuilder phoneInfo = new StringBuilder("+-------------------------------------------------+"); // Creates a StringBuilder for final output display.
+
+        System.out.print("What is the serial number? "); // Prompts user for information
+        phone1.setSerialNumber(phoneInput.nextInt()); // Assigns user submitted information to class object.
+        phoneInfo.append(String.format("\nSerial Number: %d \n", phone1.getSerialNumber())); // Adds user information to String Builder
+        phoneInput.nextLine();
+
+        System.out.print("What model is the phone? ");
+        phone1.setModel(phoneInput.nextLine());
+        phoneInfo.append(String.format("Model: %s \n", phone1.getModel()));
+
+        System.out.print("Who is the carrier? ");
+        phone1.setCarrier(phoneInput.nextLine());
+        phoneInfo.append(String.format("Carrier: %s \n", phone1.getCarrier()));
+
+        System.out.print("What is the phone number? ");
+        phone1.setPhoneNumber(phoneInput.nextLine());
+        phoneInfo.append(String.format("Phone Number: %s \n", phone1.getPhoneNumber()));
+
+        System.out.print("Who is the owner of the phone? ");
+        phone1.setOwner(phoneInput.nextLine());
+        phoneInfo.append(String.format("Owner: %s \n", phone1.getOwner()));
+        phoneInfo.append("+-------------------------------------------------+\n");
+
+        System.out.println(phoneInfo);
+
+
+    }
+
+}
