30de772a50e7139f31e34c0cd81ed224aa282029
unknown <adampjessie@gmail.com>
2025-06-09 14:25:24 -0400

    Completed dependencies exercise and NorthwindTraders exercise

--- workbook-7/nested-queries.sql
+++ workbook-7/nested-queries.sql
@@ -1,24 +1,24 @@
 -- Question 1
--- SELECT ProductName, UnitPrice
+-- SELECT ProductName
 -- FROM northwind.products
--- WHERE UnitPrice = (SELECT MAX(UnitPrice))
+-- WHERE UnitPrice = MAX(UnitPrice));
 
 -- Question 2
 -- SELECT OrderID, ShipName, ShipAddress
 -- FROM northwind.orders
--- WHERE ShipVia = (SELECT ShipperID FROM northwind.shippers WHERE CompanyName LIKE "Federal Shipping")
+-- WHERE ShipVia = (SELECT ShipperID FROM northwind.shippers WHERE CompanyName LIKE "Federal Shipping");
 
 -- Question 3
 -- SELECT OrderID
 -- FROM northwind.`order details`
--- WHERE ProductID = (SELECT ProductID FROM northwind.products WHERE ProductName LIKE "Sasquatch Ale")
+-- WHERE ProductID = (SELECT ProductID FROM northwind.products WHERE ProductName LIKE "Sasquatch Ale");
 
 -- Question 4
 -- SELECT FirstName, LastName
 -- FROM northwind.employees
--- WHERE EmployeeID = (SELECT EmployeeID FROM northwind.orders WHERE orderID = 10266)
+-- WHERE EmployeeID = (SELECT EmployeeID FROM northwind.orders WHERE orderID = 10266);
 
 -- Question 5
 -- SELECT ContactName
 -- FROM northwind.customers
--- WHERE CustomerID = (SELECT CustomerID FROM northwind.orders WHERE orderID = 10266)
+-- WHERE CustomerID = (SELECT CustomerID FROM northwind.orders WHERE orderID = 10266);
